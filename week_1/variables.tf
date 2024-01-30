fffcf8b521596331c7bcc46d873d804131e42455
Jochen Stierberger <jochen@stierberger.com>
2024-01-30 14:37:37 -0600

    add tf vars

---
+++ week_1/variables.tf
@@ -0,0 +1,29 @@
+variable "project" {
+  description = "Project"
+  default     = "dtc-de-course-412819"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "Location"
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "Dataset name"
+  default     = "demo_dataset"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket storage class"
+  default     = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "Bucket name"
+  default     = "dtc-de-course-412819"
+}
